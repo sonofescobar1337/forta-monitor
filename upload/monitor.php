3de9d43de167a51ba00ed2d664e5f6dc0cae5062
SonOfEscobar1337 <97580283+sonofescobar1337@users.noreply.github.com>
2023-04-21 18:36:34 +0700

    Update function ram usage

--- upload/monitor.php
+++ upload/monitor.php
@@ -1,14 +1,21 @@
 <?php
 $cpuUsage = sys_getloadavg()[0];
 $uptime = exec('uptime -p');
-$ramUsage = round((memory_get_usage(true) / 1024 / 1024), 2) . ' MB';
+
+$diskTotal = round((disk_total_space('/') / 1024 / 1024 / 1024), 2) . ' GB';
+$diskFree = round((disk_free_space('/') / 1024 / 1024 / 1024), 2) . ' GB';
+
+$total_ram = round(shell_exec("free -b | grep Mem | awk '{print $2}'") / 1024 / 1024);
+$free_ram = round(shell_exec("free -b | grep Mem | awk '{print $4}'") / 1024 / 1024);
+$used_ram = $total_ram - $free_ram;
 
 $data = [
-  'cpuUsage' => $cpuUsage,
-  'uptime' => $uptime,
-  'ramUsage' => $ramUsage
+    'cpuUsage' => $cpuUsage,
+    'uptime' => $uptime,
+    'usedRam' => $used_ram . ' MB',
+    'diskTotal' => $diskTotal,
+    'diskFree' => $diskFree
 ];
-
 header('Content-Type: application/json');
 echo json_encode($data);
 ?>
